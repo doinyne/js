3d7dd7272a89f7cdb29b770e7832503b9e5b8e82
doinyne <d.lewis.rave@gmail.com>
2021-12-23 16:45:00 +0000

    added comments

--- concatination.js
+++ concatination.js
@@ -1,3 +1,8 @@
+// This is an example of concatination. 
+// creating a function with name as a parameter. 
+// using he parameter to change the name.
+// calling the function with the name
+
 function sayThanks(name) {
   console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
 }
