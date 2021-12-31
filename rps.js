0eada34a1674fecc00fa954f46b1b614addb4b69
dean <d.lewis.rave@gmail.com>
2021-12-31 00:00:36 +0000

    wip

--- rps.js
+++ rps.js
@@ -1,7 +1,25 @@
 const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' ) {
   return userInput;
 } else {
   console.log('Error!');
 }
 };
+
+console.log(getUserChoice('Paper'));
+
+Math.random(Math.floor() * 3);
+
+console.log()
+
+let getComputerChoice = computerChoice => {
+  switch (randomNumber) {
+  case 0:
+    return 'rock';
+  case 1:
+    return 'paper';
+  case 2:
+    return 'scissors';
+};
+}
